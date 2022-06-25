7179939fc70ba68e5a676396c973eaa9804a2ff3
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-26 06:22:23 +0700

    Arithmetic Update

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,53 +1,78 @@
-def arithmetic_arranger(problems, val=False):
-    arranged_problems = ""
-    if len(problems) > 5:
-        arranged_problems = "Error: Too many problems."
-        return arranged_problems
+# Exception Handling function
+def exception_handling(number1, number2, operator):
+    # Only digit exception
+    try:
+        int(number1)
+    except:
+        return "Error: Numbers must only contain digits."
+    try:
+        int(number2)
+    except:
+        return "Error: Numbers must only contain digits."
+    # More than 4 digit no. exception
+    try:
+        if len(number1) > 4 or len(number2) > 4:
+            raise BaseException
+    except:
+        return "Error: Numbers cannot be more than four digits."
+    # Operator must be + | - exception.
+    try:
+        if operator != "+" and operator != "-":
+            raise BaseException
+    except:
+        return "Error: Operator must be '+' or '-'."
+    return ""
 
-    # list of all operations in str format
-    operations = list(map(lambda x: x.split()[1], problems))
-    if set(operations) != {"+", "-"} and len(set(operations)) != 2:
-        arranged_problems = "Error: Operator must be '+' or '-'."
-        return arranged_problems
 
-    numbers = []  # list of all operands in str format
-    for i in problems:
-        p = i.split()
-        numbers.extend([p[0], p[2]])
+def arithmetic_arranger(problems, displayMode=False):
 
-    if not all(map(lambda x: x.isdigit(), numbers)):
-        arranged_problems = "Error: Numbers must only contain digits."
-        return arranged_problems
-
-    if not all(map(lambda x: len(x) < 5, numbers)):
-        arranged_problems = "Error: Numbers cannot be more than four digits."
-        return arranged_problems
-
-    top_row = ""
-    dashes = ""
-    values = list(map(lambda x: eval(x), problems))
-    solutions = ""
-    for i in range(0, len(numbers), 2):
-        space_width = max(len(numbers[i]), len(numbers[i + 1])) + 2
-        top_row += numbers[i].rjust(space_width)
-        dashes += "-" * space_width
-        solutions += str(values[i // 2]).rjust(space_width)
-        if i != len(numbers) - 2:
-            top_row += " " * 4
-            dashes += " " * 4
-            solutions += " " * 4
-
-    bottom_row = ""
-    for i in range(1, len(numbers), 2):
-        space_width = max(len(numbers[i - 1]), len(numbers[i])) + 1
-        bottom_row += operations[i // 2]
-        bottom_row += numbers[i].rjust(space_width)
-        if i != len(numbers) - 1:
-            bottom_row += " " * 4
-
-    if val:
-        arranged_problems = "\n".join((top_row, bottom_row, dashes, solutions))
-    else:
-        arranged_problems = "\n".join((top_row, bottom_row, dashes))
-    return
-    
+    start = True
+    side_space = "    "
+    line1 = line2 = line3 = line4 = ""
+    # Too many Problem exception
+    try:
+        if len(problems) > 5:
+            raise BaseException
+    except:
+        return "Error: Too many problems."
+    for prob in problems:
+        # Splitting the Problem into separate strings
+        separated_problem = prob.split()
+        # storing number 1
+        number1 = separated_problem[0]
+        # Storing the operator sign
+        operator = separated_problem[1]
+        # storing number 2
+        number2 = separated_problem[2]
+        exp = exception_handling(number1, number2, operator)
+        if exp != "":
+            return exp
+        no1 = int(number1)
+        no2 = int(number2)
+        # space contains the max no. os spaces required.
+        space = max(len(number1), len(number2))
+        # For first arithmetic arragement
+        if start == True:
+            line1 += number1.rjust(space + 2)
+            line2 += operator + " " + number2.rjust(space)
+            line3 += "-" * (space + 2)
+            if displayMode == True:
+                if operator == "+":
+                    line4 += str(no1 + no2).rjust(space + 2)
+                else:
+                    line4 += str(no1 - no2).rjust(space + 2)
+            start = False
+        # Other than first arithmetic arragement
+        else:
+            line1 += number1.rjust(space + 6)
+            line2 += operator.rjust(5) + " " + number2.rjust(space)
+            line3 += side_space + "-" * (space + 2)
+            if displayMode == True:
+                if operator == "+":
+                    line4 += side_space + str(no1 + no2).rjust(space + 2)
+                else:
+                    line4 += side_space + str(no1 - no2).rjust(space + 2)
+    # displayMode is Ture then append line4
+    if displayMode == True:
+        return line1 + "\n" + line2 + "\n" + line3 + "\n" + line4
+    return line1 + "\n" + line2 + "\n" + line3
